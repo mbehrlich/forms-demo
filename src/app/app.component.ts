8d4cf2717ce7a9a9ad1fdda4b4ffb12eea09bca9
Matthew Ehrlich <mehrlich@google.com>
2020-06-22 11:46:18 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'forms-demo';
+export class AppComponent implements OnInit {
+  food = '';
+
+  submitFood() {
+    console.log(this.food);
+  }
+
+  color = new FormControl('');
+
+  ngOnInit() {
+    this.color.valueChanges.subscribe(value => {
+      console.log('Color changed: ' + value);
+    });
+  }
+
+  submitColor() {
+    console.log('Color submitted: ' + this.color.value);
+  }
+
+  STATES: string[] = ['CA', 'FL', 'MA', 'OK', 'AZ', 'TX', 'NY'];
+
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    state: new FormControl('', Validators.required),
+  });
+
+  submitProfile() {
+    console.log(this.profileForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
